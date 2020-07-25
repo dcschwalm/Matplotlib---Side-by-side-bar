84824c43a48c5b9f6979857addda3f21f5478ddc
dcschwalm <dcschwalm@pluto.dsu.edu>
2020-07-25 15:35:13 -0500

    Update script.py

--- script.py
+++ script.py
@@ -5,7 +5,6 @@ drinks = ["cappuccino", "latte", "chai", "americano", "mocha", "espresso"]
 sales1 =  [91, 76, 56, 66, 52, 27]
 sales2 = [65, 82, 36, 68, 38, 40]
 
-#Paste the x_values code here
 n = 1  # This is our first dataset (out of 2)
 t = 2 # Number of datasets
 d = 6 # Number of sets of bars
